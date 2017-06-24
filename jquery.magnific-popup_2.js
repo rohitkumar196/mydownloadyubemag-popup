6d827430ae2add9b2774d5d08342dff808b44aa5
rohitkumar196 <rk919688@gmail.com>
2017-06-25 03:12:40 +0530

    Update jquery.magnific-popup_2.js

--- jquery.magnific-popup_2.js
+++ jquery.magnific-popup_2.js
@@ -1,6 +1,6 @@
-/*! Magnific Popup - v1.1.0 - 2016-02-20
-* http://dimsemenov.com/plugins/magnific-popup/
-* Copyright (c) 2016 Dmitry Semenov; */
+/*! Magnific Popup - v1.0.0 - 2017-06-25
+* http://mydownloadtube.tk/
+* Copyright (c) 2017  */
 ;(function (factory) { 
 if (typeof define === 'function' && define.amd) { 
  // AMD. Register as an anonymous module. 
